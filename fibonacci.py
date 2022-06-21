b3c92e6b13f46ef7505cbbe2896f6b17e7750c03
JoseCastanoCoding <j.albertocast@gmail.com>
2022-06-21 00:02:20 +0000

    initial commit

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+
+
+for i in range(0, 50):
+
+    fib_first_3 = [0, 1, 1]
+
+    fib_sum = fib_first_3[1] + fib_first_3[2]
+
+    next_number = fib_first_3.append(fib_sum)
+
+    print(fib_first_3)
+
